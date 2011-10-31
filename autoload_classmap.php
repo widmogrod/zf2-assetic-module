a6ddba8b1447ddc8a041043b9566bfcf45f4eeab
Gabriel <widmogrod@gmail.com>
2011-10-31 18:43:53 +0100

    update aultoload_classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -3,6 +3,8 @@ return array (
     'Assetic\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
     'Assetic\Controller\AsseticExampleController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Assetic' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'AsseticExample.php',
 
-    'Assetic\Service\Service' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Assetic' . DIRECTORY_SEPARATOR . 'Service' . DIRECTORY_SEPARATOR . 'Service.php',
+    'Assetic\Service' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Assetic' . DIRECTORY_SEPARATOR . 'Service.php',
+    'Assetic\Configuration' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Assetic' . DIRECTORY_SEPARATOR . 'Configuration.php',
+
 //    'Assetic\\Asset\\AssetCollection' => __DIR__ . '/library/assetic/src/Assetic/Asset/AssetCollection.php'
 );
