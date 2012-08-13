65088d31b91b37fc24d15b833d597e1943529bd8
Celtico <cel@cenics.net>
2012-08-13 16:18:46 +0300

    Update configs/module.config.php

--- configs/module.config.php
+++ configs/module.config.php
@@ -11,41 +11,4 @@ return array(
             'Assetic\FilterManager' => 'AsseticBundle\SimpleFactory',
         ),
     ),
-
-    'assetic_configuration' => array(
-        'debug' => false,
-        'webPath' => __DIR__ . '/../../../public/assets',
-        'baseUrl' => '/assets',
-        'strategyForRenderer' => array(
-            'AsseticBundle\View\ViewHelperStrategy' => 'Zend\View\Renderer\PhpRenderer'
-        ),
-        'routes' => array(
-            'application' => array(
-                '@base_css',
-                '@base_js',
-            ),
-            'home' => array(
-                '@base_css',
-            ),
-        ),
-        'modules' => array(
-            'application' => array(
-                'root_path' => '/Users/cel/Desktop/ZendSkeletonApplication/module/Application/assets',
-                'collections' => array(
-                    'base_css' => array(
-                        'assets' => array(
-                            'css/global.css',
-                            'css/*.css',
-                        ),
-                    ),
-                    'base_js' => array(
-                        'assets' => array(
-                            'http://code.jquery.com/jquery-1.5.2.min.js',
-                            'js/test.js',
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
-);
+);    
