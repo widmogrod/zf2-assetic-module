384e9f098ef9188ccdd88b6b76062694623f3515
vahid sohrabloo <vahid4134@gmail.com>
2012-09-28 15:59:15 +0330

    add class map for load php less

--- Module.php
+++ Module.php
@@ -73,6 +73,9 @@ class Module implements InitProviderInterface, AutoloaderProviderInterface, Conf
     public function getAutoloaderConfig()
     {
         return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__ . '/',
