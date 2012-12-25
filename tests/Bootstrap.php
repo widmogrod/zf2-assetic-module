cfcf35cbee8373dfe2b6cc7dad4ae79907eeb1d4
widmogrod <widmogrod@gmail.com>
2012-12-25 11:46:48 +0100

    prepare to add tests

---
+++ tests/Bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__ . '/../library');
+
+/**
+ * Simple autoloader
+ */
+spl_autoload_register(function($className){
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    if ($lastNsPos = strripos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
+});
