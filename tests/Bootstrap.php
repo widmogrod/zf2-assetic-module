a5c5b1e9a3740c1fa3841b14ed37e241ff5ba172
widmogrod <widmogrod@gmail.com>
2013-05-06 18:11:36 +0200

    update Bootstrap & trawis.yml

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -6,18 +6,4 @@ define('TEST_ASSETS_DIR', __DIR__ . '/assets');
 define('TEST_CACHE_DIR', __DIR__ . '/cache');
 define('TEST_PUBLIC_DIR', __DIR__ . '/public');
 
-/**
- * Simple autoloader
- */
-spl_autoload_register(function($className){
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    if ($lastNsPos = strripos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    require $fileName;
-});
+require_once __DIR__ . '/../vendor/autoload.php';
