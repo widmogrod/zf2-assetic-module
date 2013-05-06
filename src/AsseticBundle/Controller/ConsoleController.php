b37904c3e21743e7af0211d734264f4cf7ed0fc2
widmogrod <widmogrod@gmail.com>
2013-05-06 19:42:07 +0200

    create console controller

--- src/AsseticBundle/Controller/ConsoleController.php
+++ src/AsseticBundle/Controller/ConsoleController.php
@@ -14,12 +14,36 @@ class ConsoleController extends AbstractActionController implements AsseticBundl
 
     public function buildAction()
     {
-        $this->assetic->initLoadedModules();
+        $this->assetic->build();
     }
 
     public function setupAction()
     {
+        $config = $this->assetic->getConfiguration();
+        $mode = (null !== ($mode = $config->getUmask())) ? $mode : 0775;
+        $displayMode = decoct($mode);
 
+        $cachePath = $config->getCachePath();
+        $pathExists = is_dir($cachePath);
+        if ($cachePath && !$pathExists) {
+            mkdir($cachePath, $mode, true);
+            echo "Cache path created '$cachePath' with mode '$displayMode' \n";
+        } else if ($pathExists) {
+            echo "Creation of cache path '$cachePath' skipped - path exists \n";
+        } else {
+            echo "Creation of cache path '$cachePath' skipped - no path provided \n";
+        }
+
+        $webPath = $config->getWebPath();
+        $pathExists = is_dir($webPath);
+        if ($webPath && !$pathExists) {
+            mkdir($webPath, $mode, true);
+            echo "Web path created '$webPath' with mode '$displayMode' \n";
+        } else if ($pathExists) {
+            echo "Creation of cache path '$webPath' skipped - path exists \n";
+        } else {
+            echo "Creation of cache path '$webPath' skipped - no path provided \n";
+        }
     }
 
     public function setAsseticBundleService(Service $service)
