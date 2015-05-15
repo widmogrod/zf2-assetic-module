9995bffb81a492f1caa5c87c5c1d41a3c4bf735b
widmogrod <widmogrod@gmail.com>
2015-05-15 21:42:12 +0200

    View\Helper\Asset now has entry in module.config.php

--- configs/module.config.php
+++ configs/module.config.php
@@ -1,46 +1,53 @@
 <?php
 use Zend\Mvc\Application;
 
-return array(
-    'controllers' => array(
-        'invokables' => array(
+return [
+    'controllers' => [
+        'invokables' => [
             'AsseticBundle\Controller\Console' => 'AsseticBundle\Controller\ConsoleController',
-        ),
-        'initializers' => array(
+        ],
+        'initializers' => [
             'AsseticBundleInitializer' => 'AsseticBundle\Initializer\AsseticBundleInitializer',
-        ),
-    ),
-
-    'service_manager' => array(
-        'aliases' => array(
+        ],
+    ],
+    'service_manager' => [
+        'aliases' => [
             'AsseticConfiguration' => 'AsseticBundle\Configuration',
             'AsseticService' => 'AsseticBundle\Service',
             'Assetic\FilterManager' => 'AsseticBundle\FilterManager',
-        ),
-        'factories' => array(
+        ],
+        'factories' => [
             'AsseticBundle\Service' => 'AsseticBundle\ServiceFactory',
+            'AsseticBundle\Configuration' => 'AsseticBundle\ConfigurationFactory',
             'Assetic\AssetWriter' => 'AsseticBundle\WriterFactory',
-        ),
-        'invokables' => array(
-            'Assetic\AssetManager'   => 'Assetic\AssetManager',
-            'AsseticBundle\FilterManager'  => 'AsseticBundle\FilterManager',
+        ],
+        'invokables' => [
+            'Assetic\AssetManager' => 'Assetic\AssetManager',
+            'AsseticBundle\FilterManager' => 'AsseticBundle\FilterManager',
             'AsseticBundle\Listener' => 'AsseticBundle\Listener',
-        ),
-        'initializers' => array(
+        ],
+        'initializers' => [
             'AsseticBundleInitializer' => 'AsseticBundle\Initializer\AsseticBundleInitializer',
-        ),
-    ),
-
-    'assetic_configuration' => array(
-        'rendererToStrategy' => array(
+        ],
+    ],
+    'view_helpers' => [
+        'aliases' => [
+            'asset' => 'AsseticBundle\View\Helper\Asset',
+        ],
+        'factories' => [
+            'AsseticBundle\View\Helper\Asset' => 'AsseticBundle\View\Helper\AssetFactory'
+        ],
+    ],
+    'assetic_configuration' => [
+        'rendererToStrategy' => [
             'Zend\View\Renderer\PhpRenderer' => 'AsseticBundle\View\ViewHelperStrategy',
             'Zend\View\Renderer\FeedRenderer' => 'AsseticBundle\View\NoneStrategy',
             'Zend\View\Renderer\JsonRenderer' => 'AsseticBundle\View\NoneStrategy',
-        ),
-        'acceptableErrors' => array(
+        ],
+        'acceptableErrors' => [
             Application::ERROR_CONTROLLER_NOT_FOUND,
             Application::ERROR_CONTROLLER_INVALID,
             Application::ERROR_ROUTER_NO_MATCH
-        ),
-    ),
-);
+        ],
+    ],
+];
