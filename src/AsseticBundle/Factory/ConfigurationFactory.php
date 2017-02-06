4fe9ddc1177f00d9efc11d83cf6d5c6b121984e8
Olivier MELLINGER <parktrip@gmail.com>
2017-02-06 14:15:47 +0100

    add space (cs-fixer)

--- src/AsseticBundle/Factory/ConfigurationFactory.php
+++ src/AsseticBundle/Factory/ConfigurationFactory.php
@@ -10,6 +10,7 @@ class ConfigurationFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $configuration = $container->get('Configuration');
+        
         return new Configuration($configuration['assetic_configuration']);
     }
 }
