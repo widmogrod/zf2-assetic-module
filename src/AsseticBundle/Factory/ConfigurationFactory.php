6f745072102071c13825875a4a116c5958057400
Olivier MELLINGER <parktrip@gmail.com>
2017-02-06 14:22:05 +0100

    Update ConfigurationFactory.php

--- src/AsseticBundle/Factory/ConfigurationFactory.php
+++ src/AsseticBundle/Factory/ConfigurationFactory.php
@@ -10,7 +10,6 @@ class ConfigurationFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $configuration = $container->get('Configuration');
-        
         return new Configuration($configuration['assetic_configuration']);
     }
 }
