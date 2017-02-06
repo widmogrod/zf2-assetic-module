7e41101b9e32c5120d10c081b70ab00a5ee20e9f
Olivier MELLINGER <parktrip@gmail.com>
2017-02-06 09:08:13 +0100

    Create Configuration factory

---
+++ src/AsseticBundle/Factory/ConfigurationFactory.php
@@ -0,0 +1,15 @@
+<?php
+namespace AsseticBundle\Factory;
+
+use AsseticBundle\Configuration;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class ConfigurationFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $configuration = $container->get('Configuration');
+        return new Configuration($configuration['assetic_configuration']);
+    }
+}
