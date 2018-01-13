32622c59a2daf46408c3a548fefc1eef8b7dbe40
Vasily Belosludcev <bupy765@gmail.com>
2018-01-13 18:31:22 +0500

    AsseticBundle\CacheBuster\Null has deprecated.

--- src/AsseticBundle/CacheBuster/Null.php
+++ src/AsseticBundle/CacheBuster/Null.php
@@ -2,13 +2,27 @@
 
 namespace AsseticBundle\CacheBuster;
 
-use Assetic\Asset\AssetInterface,
-    Assetic\Factory\Worker\WorkerInterface,
-    Assetic\Factory\AssetFactory;
-
-class Null implements WorkerInterface
+/**
+ * Stub class for backwards compatibility.
+ *
+ * Since PHP 7 adds "null" as a reserved keyword, we can no longer have a class
+ * named that and retain PHP 7 compatibility. The original class has been
+ * renamed to "NoCache", and this class is now an extension of it. It raises an
+ * E_USER_DEPRECATED to warn users to migrate.
+ *
+ * @deprecated
+ */
+class Null extends NoCache
 {
-    public function process(AssetInterface $asset, AssetFactory $factory)
+    public function __construct($typeOrOptions = null)
     {
+        trigger_error(
+            sprintf(
+                'The class %s has been deprecated; please use %s\\NoCache',
+                __CLASS__,
+                __NAMESPACE__
+            ),
+            E_USER_DEPRECATED
+        );
     }
 }
